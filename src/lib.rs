bbf0d49a0d635ba7cd469600b97448213838b48a
dkaps125 <daniel.kapit05@gmail.com>
2017-10-11 17:15:12 -0400

    Added rust screen writing

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,31 @@
 #![feature(lang_items)]
+#![feature(const_fn)]
+#![feature(unique)]
+#![feature(const_unique_new)]
 #![no_std]
 
 extern crate rlibc;
+extern crate volatile;
+mod vga_buffer;
+
+use vga_buffer::p;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let x = ["Hello", "world", "!"];
-    let y = x;
+    let hello = b"Hello world!";
+    let color_byte = 0x1f;
+
+    let mut hello_colored = [color_byte; 24];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i*2] = *char_byte;
+    }
+
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored };
+
+    p();
+
+    loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
